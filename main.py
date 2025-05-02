91b6a5247ed0b19f2d79434a1e12c027b15216dd
erkanyurtoglu <158731690+erkanyurtoglu@users.noreply.github.com>
2025-05-02 19:45:35 +0300

    yeni dosyalar eklendi
    main.py , character.py , constants.py dosyaları oluşturuldu. Giriş kodları yazıldı.

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-print("ceren ekleme yaptı")
+import pygame
+import constants
+from character import Character
 
-print("Nisa ekleme yaptı")
-print("hello world")
+pygame.init()
+
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+pygame.display.set_caption("Dungeon Crawler")
+
+#create player
+player = Character(100, 100)
+
+#main game loop
+run = True
+while run:
+
+  #draw player on screen
+  player.draw(screen)
+
+  #event handler
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+
+  pygame.display.update()
+
+pygame.quit()
