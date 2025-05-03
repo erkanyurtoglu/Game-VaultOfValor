ee38683b587181fb50815f42ced485789b16df82
erkanyurtoglu <158731690+erkanyurtoglu@users.noreply.github.com>
2025-05-03 16:27:13 +0300

    Karakter ve Animasyon
    Karakter eklendi ve w,a,s,d tuşları ile sağ, sol, ileri, geri hareketi sağlandı

--- character.py
+++ character.py
@@ -3,18 +3,38 @@ import constants
 import math
 
 class Character():
-  def __init__(self, x, y):
+  def __init__(self, x, y, animation_list):
+    self.flip = False
+    self.animation_list = animation_list
+    self.frame_index = 0
+    self.update_time = pygame.time.get_ticks()
+    self.image = animation_list[self.frame_index]
     self.rect = pygame.Rect(0, 0, 40, 40)
     self.rect.center = (x, y)
 
-    def move(self, dx, dy):
-      # control diagonal speed
-      if dx != 0 and dy != 0:
-        dx = dx * (math.sqrt(2) / 2)
-        dy = dy * (math.sqrt(2) / 2)
+  def move(self, dx, dy):
 
-      self.rect.x += dx
-      self.rect.y += dy
+    if dx < 0:
+      self.flip = True
+    if dx > 0:
+      self.flip = False
+    if dx != 0 and dy != 0:
+      dx = dx * (math.sqrt(2) / 2)
+      dy = dy * (math.sqrt(2) / 2)
+
+    self.rect.x += dx
+    self.rect.y += dy
+
+  def update(self):
+    animation_cooldown = 70
+    self.image = self.animation_list[self.frame_index]
+    if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+     self.frame_index += 1
+     self.update_time = pygame.time.get_ticks()
+    if self.frame_index >= len(self.animation_list):
+      self.frame_index = 0
 
   def draw(self, surface):
-    pygame.draw.rect(surface, constants.RED, self.rect)
+    flipped_image = pygame.transform.flip(self.image, self.flip, False)
+    surface.blit(flipped_image, self.rect)
+    pygame.draw.rect(surface, constants.RED, self.rect, 1)
--- main.py
+++ main.py
@@ -5,30 +5,37 @@ from character import Character
 pygame.init()
 
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-pygame.display.set_caption("Dungeon Crawler")
+pygame.display.set_caption("Vault Of Valor")
 
-#create clock for maintaining frame rate
 clock = pygame.time.Clock()
 
-#define player movement variables
 moving_left = False
 moving_right = False
 moving_up = False
 moving_down = False
 
-#create player
-player = Character(100, 100)
+def scale_img(image, scale):
+  w = image.get_width()
+  h = image.get_height()
+  return pygame.transform.scale(image, (w * scale, h * scale))
 
-#main game loop
+animation_list = []
+for i in range(4):
+  img = pygame.image.load(f"assets/images/characters/elf/idle/{i}.png").convert_alpha()
+  img = scale_img(img, constants.SCALE)
+  animation_list.append(img)
+
+
+player = Character(100, 100, animation_list)
+
+
 run = True
 while run:
 
-  # control frame rate
   clock.tick(constants.FPS)
 
   screen.fill(constants.BG)
 
-  # calculate player movement
   dx = 0
   dy = 0
   if moving_right == True:
@@ -40,17 +47,17 @@ while run:
   if moving_down == True:
     dy = constants.SPEED
 
-    # move player
-    player.move(dx, dy)
 
-  #draw player on screen
+  player.move(dx, dy)
+
+  player.update()
+
   player.draw(screen)
 
-  #event handler
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
-      # take keyboard presses
+
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_a:
         moving_left = True
@@ -61,7 +68,6 @@ while run:
       if event.key == pygame.K_s:
         moving_down = True
 
-      # keyboard button released
     if event.type == pygame.KEYUP:
       if event.key == pygame.K_a:
         moving_left = False
