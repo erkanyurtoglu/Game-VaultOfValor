d70a38b274f517376773b0dcad25e288326f6a5e
tuncaynisa <nisatuncay2@gmail.com>
2025-05-03 12:21:17 +0300

    deneme

--- main.py
+++ main.py
@@ -7,12 +7,41 @@ pygame.init()
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 pygame.display.set_caption("Dungeon Crawler")
 
+#create clock for maintaining frame rate
+clock = pygame.time.Clock()
+
+#define player movement variables
+moving_left = False
+moving_right = False
+moving_up = False
+moving_down = False
+
 #create player
 player = Character(100, 100)
 
 #main game loop
 run = True
 while run:
+
+  # control frame rate
+  clock.tick(constants.FPS)
+
+  screen.fill(constants.BG)
+
+  # calculate player movement
+  dx = 0
+  dy = 0
+  if moving_right == True:
+    dx = constants.SPEED
+  if moving_left == True:
+    dx = -constants.SPEED
+  if moving_up == True:
+    dy = -constants.SPEED
+  if moving_down == True:
+    dy = constants.SPEED
+
+    # move player
+    player.move(dx, dy)
 
   #draw player on screen
   player.draw(screen)
@@ -21,6 +50,27 @@ while run:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
+      # take keyboard presses
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_a:
+        moving_left = True
+      if event.key == pygame.K_d:
+        moving_right = True
+      if event.key == pygame.K_w:
+        moving_up = True
+      if event.key == pygame.K_s:
+        moving_down = True
+
+      # keyboard button released
+    if event.type == pygame.KEYUP:
+      if event.key == pygame.K_a:
+        moving_left = False
+      if event.key == pygame.K_d:
+        moving_right = False
+      if event.key == pygame.K_w:
+        moving_up = False
+      if event.key == pygame.K_s:
+        moving_down = False
 
   pygame.display.update()
 
